d170f3d4f3e7ef70e202fe66dd900419ff9aeec4
venuch8179@gmail.com <devopsvenu98@gmail.com>
2025-12-08 12:02:54 +0530

    Pushing aws-eks project and modules

---
+++ OneDrive/Desktop/Cloud_nginx_project/modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "environment" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "azs" {
+  type = list(string)
+}
